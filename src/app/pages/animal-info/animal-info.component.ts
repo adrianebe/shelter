d21a16ffe76e365089ef69d0b8f9026ff1ec717d
Adrian <adrian.bilski2b@gmail.com>
2024-03-18 11:51:24 +0100

    szablon v0

---
+++ src/app/pages/animal-info/animal-info.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-animal-info',
+  standalone: true,
+  imports: [],
+  templateUrl: './animal-info.component.html',
+  styleUrl: './animal-info.component.scss'
+})
+export class AnimalInfoComponent {
+
+}
